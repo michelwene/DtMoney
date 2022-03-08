7c6ce76fc478a0142e585a3cf6944a906cf52aad
Michel Wene <michelwene@hotmail.com>
2022-03-08 14:34:22 -0300

    add transaction from  api to table

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de Website",
+          type: "deposit",
+          category: "Dev",
+          amount: "6000",
+          createdAt: new Date("2021-05-05 12:57:53"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: "1100",
+          createdAt: new Date("2021-05-22 11:30:22"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "transactions 1",
-          amount: 400,
-          type: 'deposit',
-          category: 'food',
-          createdAt: new Date(),
-        },
-      ];
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create("transaction", data);
     });
   },
 });
