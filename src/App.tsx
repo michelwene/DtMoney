3dbeff4780f27efb8ef76d0742184278a6bedf77
Michel Wene <michelwene@hotmail.com>
2022-03-08 16:59:41 -0300

    add values at dashboard in Summary

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,7 @@ import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-import {
-  TransactionsContext,
-  TransactionsProvider,
-} from "./TransactionsContext";
+import { TransactionsProvider } from "./hooks/useTransactions";
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement("#root");
